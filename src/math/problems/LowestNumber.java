fe2433ccdee189fae2d38583590f0a36109ccb3b
imran6th <imran6th@hotmail.com>
2018-12-26 19:35:07 -0500

    math problems

--- src/math/problems/LowestNumber.java
+++ src/math/problems/LowestNumber.java
@@ -2,7 +2,13 @@ package math.problems;
 
 import databases.ConnectToSqlDB;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class LowestNumber {
@@ -13,11 +19,12 @@ public class LowestNumber {
 		 * Use one of the databases from mysql or mongodb to store and to retrieve.
 		 */
 		int  array[] = new int[]{211,110,99,34,67,89,67,456,321,456,78,90,45,32,56,78,90,54,32,123,67,5,679,54,32,65};
-
 		//find lowest number from the array
+		Arrays.sort(array);
 
 		ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
 		List<String> lowestValue = new ArrayList<String>();
+		//lowestValue.add(Integer.toString(array[0]));
 		try {
 			connectToSqlDB.insertDataFromArrayToSqlTable(array, "tbl_lowestNumber", "column_lowestNumber");
 			lowestValue = connectToSqlDB.readDataBase("tbl_lowestNumber", "column_lowestNumber");
@@ -25,10 +32,11 @@ public class LowestNumber {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		System.out.println("Data is reading from the Table (tbl_primenumber) and displaying to the console");
+		System.out.println("Data is reading from the Table (tbl_lowestnumber) and displaying to the console");
 		for(String st:lowestValue){
 			System.out.println(st);
 		}
+		System.out.println("Lowest number in this array is  "+array[0]);
 	}
 
 }
