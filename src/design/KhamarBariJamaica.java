d6a73d6267c650caedfd483c4603c25e6f773acf
imran6th <imran6th@hotmail.com>
2018-12-27 20:03:54 -0500

    final

---
+++ src/design/KhamarBariJamaica.java
@@ -0,0 +1,7 @@
+package design;
+
+public class KhamarBariJamaica extends EmployeeInfo {
+    public static void KhamarBariJamaicainfo(){
+        System.out.println("Khamar Bari Jamaica, Address: Hillside, Jamaica, NY \n");
+    }
+}
--- src/json/parser/CnnAPI.java
+++ src/json/parser/CnnAPI.java
@@ -4,8 +4,8 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import databases.ConnectToMongoDB;
-import parser.Student;
+import databases.ConnectToSqlDB;
+
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,14 +15,14 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
-
-public class CnnAPI {
+import databases.ConnectToSqlDB;
+class CnnAPI {
     /*
       You can get API_KEY from this below link. Once you have the API_KEY, you can fetch the top-headlines news.
       https://newsapi.org/s/cnn-api
       Fetch This following CNN API, It will return some news in Json data. Parse this data and construct
-      https://newsapi.org/v2/top-headlines?sources=cnn&apiKey=01502dde174d44e1aa6c1dfa616e967e
-      MY_API_KEY=01502dde174d44e1aa6c1dfa616e967e
+      https://newsapi.org/v2/top-headlines?sources=cnn&apiKey=YOUR_API_KEY
+      MY_API_KEY=0d9e35dfa3c140aab8bf9cdd70df957f
       After getting Json Format of the news, You can go to json validator link: https://jsonlint.com/ to see
       how it can be parsed.
       "articles": [{
@@ -39,64 +39,69 @@ public class CnnAPI {
 		"content": "Chat with us in Facebook Messenger. Find out what's happening in the world as it unfolds."
 	   },{}]
 	   Read the articles array and construct Headline news as source, author, title,description,url,urlToImage,publishedAt
-	   and content. You need to design News Data Model and construct headline news.
+	   and content. You need to design123 News Data Model and construct headline news.
 	   You can store in Map and then into ArrayList as your choice of Data Structure.
 	   You can follow How we implemented in Employee and JsonReaderUtil task.
 	   Show output of all the headline news in to console.
 	   Store into choice of your database and retrieve.
      */
 
-    public static void main(String[] args) throws MalformedURLException, IOException {
-        String sURL = "https://newsapi.org/v2/top-headlines?sources=cnn&apiKey=01502dde174d44e1aa6c1dfa616e967e";
-        HelperClass methods = null;
-        List<HelperClass> newsList = new ArrayList<>();
-        URL url = new URL(sURL);
-        URLConnection request = url.openConnection();
+    public static void main(String[] args) throws Exception {
+        String sURL = "https://newsapi.org/v2/top-headlines?sources=cnn&apiKey=0d9e35dfa3c140aab8bf9cdd70df957f";
+        NewsDataModel news = null;
+        List<NewsDataModel> list1 = new ArrayList<>();
+        URL url1 = new URL(sURL);
+        URLConnection request = url1.openConnection();
         request.connect();
         JsonArray jsonArray = null;
+        JsonObject rootObj = null;
         JsonParser jp = new JsonParser();
         JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
-        JsonObject xd = new JsonObject();
-        xd.add("articles", root);
-        //Create ConnectToSqlDB Object
-        ConnectToMongoDB connectToMongoDB = new ConnectToMongoDB();
+        if (root instanceof JsonObject) {
+            rootObj = root.getAsJsonObject();
+        } else if (root instanceof JsonArray) {
+            jsonArray =  root.getAsJsonArray();
+        }
 
-        jsonArray = new JsonArray();
-        jsonArray.add(root.getAsJsonObject().get("articles"));
+        //if root is an Json object
+        if(jsonArray==null)
+            jsonArray= rootObj.getAsJsonArray("articles");
+
+        //initialize field to construct HeadlineNews ogject
+        String  source = null;
+        String  author = null;
+        String title = null;
+        String description = null;
+        String url = null ;
+        String urlToImage = null ;
+        String publishedAt = null ;
+        String  content = null;
 
 
-        for (int i = 0; i < jsonArray.get(0).getAsJsonArray().size(); i++) {
+        for (int i = 0; i < jsonArray.size()-1; i++) {
             try {
-                JsonObject jsonobject = jsonArray.get(0).getAsJsonArray().get(i).getAsJsonObject();
+                JsonObject jsonobject = jsonArray.get(i).getAsJsonObject();
 
-                String source = jsonobject.get("source").getAsJsonObject().get("id").toString();
-                String author = jsonobject.get("author").toString();
-                String title = jsonobject.get("title").toString();
-                String description = jsonobject.get("description").toString();
-                String u = jsonobject.get("url").toString();
-                String urlToImage = jsonobject.get("urlToImage").toString();
-                String publisherAt = jsonobject.get("publishedAt").toString();
-                String content = jsonobject.get("content").toString();
+                source= jsonobject.get("source").toString();
+                author= jsonobject.get("author").toString();
+                title = jsonobject.get("title").toString();
+                description = jsonobject.get("description").toString();
+                url= jsonobject.get("url").toString();
+                urlToImage= jsonobject.get("urlToImage").toString();
+                publishedAt = jsonobject.get("publishedAt").toString();
+                content = jsonobject.get("content").toString();
 
-                methods = new HelperClass(source, author, title, description, u, urlToImage, publisherAt, content);
+                //Object of HeadlineNews
+                news = new NewsDataModel(source,author,title,description,url,urlToImage,publishedAt,content);
+                list1.add(news);
 
-                newsList.add(methods);
 
-            } catch (Exception ex) {
+            }catch(Exception ex){
+
             }
         }
-
-        //Print to the console.
-        for (HelperClass entry : newsList) {
-            System.out.println(entry.getSource() + " " + entry.getAuthor() + " " + entry.getTitle() + " " + entry.getDescription() + " " + entry.getUrl() + " " + entry.getUrlToImage() + " " + entry.getPublisherAt() + " " + entry.getContent());
-        }
-
-        connectToMongoDB.newInsertIntoMongoDB(newsList, "CNNApi");
-        // print form data base to console
-        List<HelperClass> st1List = connectToMongoDB.newReadListFromMongoDB(newsList, "CNNApi");
-        for (HelperClass entry : st1List) {
-            System.out.println(entry.getSource() + " " + entry.getAuthor() + " " + entry.getTitle() + " " + entry.getDescription() + " " + entry.getUrl() + " " + entry.getUrlToImage() + " " + entry.getPublisherAt() + " " + entry.getContent());
-        }
+        //Storing to Database
 
     }
+
 }
